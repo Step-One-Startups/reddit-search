b230e76c78459eca09be0b53a39abced13aa303a
David Corbitt <davidlcorbitt@gmail.com>
2023-02-26 11:28:35 -0800

    Add search_posts_raw

--- reddit/search.py
+++ reddit/search.py
@@ -2,6 +2,7 @@ from typing import List
 from reddit.archiver import Archiver
 from reddit.configuration import Configuration
 from reddit.bdfr.logger import make_console_logging_handler, silence_module_loggers, logger
+import requests
 
 
 def search_posts(config: Configuration):
@@ -18,3 +19,27 @@ def search_posts(config: Configuration):
         logger.info("Search complete")
         return posts
 
+def search_posts_raw(problem: str):
+    posts = []
+    try:
+        search_string = f"http://www.reddit.com/search.json?q={problem}"
+        print(search_string)
+        response = requests.get(search_string, headers = {'User-agent': 'step-one bot 0.1'}).json()
+        raw_posts = response["data"]["children"]
+        # print(posts)
+        # print("\n\n\n")
+        # print(posts[0])
+        for raw_post in raw_posts:
+            posts.append({
+                "title": raw_post["data"]["title"],
+                "subreddit": raw_post["data"]["subreddit"],
+                "selftext": raw_post["data"]["selftext"],
+                "permalink": raw_post["data"]["permalink"],
+            })
+        print(raw_posts[0]["data"]["subreddit"])
+    except Exception:
+        logger.exception("search_posts_raw exited unexpectedly")
+        raise
+    else:
+        logger.info("Search complete")
+        return posts
